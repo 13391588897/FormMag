3e106e0ba0e35494bb773bb0d210e8b4ff1a4f2f
zhenghao@bestseller.com.cn <919811679@qq.com>
2020-03-23 14:00:42 +0800

    更新export.js

--- H5/bs/js/export.js
+++ H5/bs/js/export.js
@@ -55,16 +55,25 @@ function methodExport(tableid) {
 function Cleanup() {
 	window.clearInterval(idTmr);
 	CollectGarbage();
-}
+};
 var tableToExcel = (function() {
-	var uri = 'data:application/vnd.ms-excel;base64,', template = '<html><head><meta charset="UTF-8"></head><body><table  border="1">{table}</table></body></html>', base64 = function(
-	s) {
-		return window.btoa(unescape(encodeURIComponent(s)))
-	}, format = function(s, c) {
-		return s.replace(/{(\w+)}/g, function(m, p) {
-			return c[p];
-		})
-	}
+	var uri = 'data:application/vnd.ms-excel;base64,',
+		/*template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',*/
+		template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel"' +
+            'xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>'
+            + '<x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets>'
+            + '</x:ExcelWorkbook></xml><![endif]-->'
+            + '<meta charset="UTF-8">' 
+            + '</head><body ><table class="excelTable">{table}</table></body></html>',
+		base64 = function(s) {
+			return window.btoa(unescape(encodeURIComponent(s)))
+		},
+		//替换table数据和worksheet名字
+		format = function(s, c) {
+			return s.replace(/{(\w+)}/g, function(m, p) {
+				return c[p];
+			})
+		}
 	return function(table, name) {
 		if (!table.nodeType)
 		table = document.getElementById(table)
